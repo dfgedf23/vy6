f89affc8c4f93d06705b29e74bf30f181d7c6c0b
dfgedf23 <93518808+dfgedf23@users.noreply.github.com>
2022-11-14 09:57:14 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=8edd15ba-d3ce-4bd2-9608-6aa153e96d0a
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=clickspeedtest.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
